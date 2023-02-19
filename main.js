8837aa74b05e722373fe096103eec86eff5ebdb8
CrashXD <crashxd@inbox.ru>
2023-02-19 21:06:35 +0600

    init

---
+++ main.js
@@ -0,0 +1,3 @@
+let users = ['Isabella', 'Lucas', 'Sophia', 'Noah', 'Ava', 'Ethan', 'Emma', 'Liam', 'Olivia', 'Mason'];
+let names = ['Ethan', 'Lucas', 'Charlotte', 'Logan', 'Mia', 'Emma', 'Amelia', 'Jackson', 'Harper', 'Olivia'];
+let students = ['Ava', 'Mia', 'Jax', 'Zoe'];
